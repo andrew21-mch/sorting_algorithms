af4df4a6776748e446b6483fbb6307bdbd0ad8b4
andrew21-mch <nfonandrew73@gmail.com>
2021-10-27 18:22:30 +0100

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,23 +9,24 @@
 void bubble_sort(int *array, size_t size)
 {
 size_t i, len = size;
-short int swapped;
-int tmp;
+short int swapp_value;
+int temp;
 
 do {
-	swapped = 0;
+	swapp_value = 0;
 	for (i = 1; i < size; i++)
 	{
 		if (array[i - 1] > array[i])
 		{
-			tmp = array[i];
+		  //the swapp functionality here
+			temp = array[i];
 			array[i] = array[i - 1];
-			array[i - 1] = tmp;
-			swapped = i;
+			array[i - 1] = temp;
+			swapp_value = i;
 			print_array(array, len);
 		}
 	}
-	size = swapped;
+	size = swapp_value;
 } while (size >= 1);
 
 }
